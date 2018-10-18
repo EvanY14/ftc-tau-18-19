e6c1c7d6f0e521606ec7f5b5edd0501969768939
Csun01 <42942891+Csun01@users.noreply.github.com>
2018-10-17 19:28:05 -0500

    Add files via upload

--- Teleop.java
+++ Teleop.java
@@ -11,95 +11,115 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
-/**
- * Created by Evan Yu on 9/16/2018.
- */
-@TeleOp(name = "Tau Teleop", group = "Tau")
-public class Teleop extends OpMode{
-    Hardware robot = new Hardware();
+    /**
+     * Created by Evan Yu on 9/16/2018.
+     */
+    @TeleOp(name = "Tau Teleop", group = "Tau")
+    public class Teleop extends OpMode{
+        Hardware robot = new Hardware();
 
-    //Drive variables
-    private double leftGP1X = 0;
-    private double leftGP1Y = 0;
-    private double rightGP1X = 0;
-    private double rightGP1Y = 0;
-    private double GP2X = 0;
-    private double GP2Y = 0;
-    private double frontleftPOWER = 0;
-    private double frontrightPOWER = 0;
-    private double backleftPOWER = 0;
-    private double backrightPOWER = 0;
-    private double maxPOWER = 0;
-
-
-    @Override
-    public void init() {
-        telemetry.addData("Readiness", "NOT READY TO START, PLEASE WAIT");
-        updateTelemetry(telemetry);
-
-        robot.init(hardwareMap);
-
-        telemetry.addData("Readiness", "Press Play to start");
-        //telemetry.addData("If you notice this", "You are COOL!!! (Evan was here)");
-        updateTelemetry(telemetry);
-    }
-
-    @Override
-    public void init_loop()
-    {
-
-    }
+        //Drive variables
+        private double leftGP1X = 0;
+        private double leftGP1Y = 0;
+        private double rightGP1X = 0;
+        private double rightGP1Y = 0;
+        private double GP2X = 0;
+        private double GP2Y = 0;
+        private double frontleftPOWER = 0;
+        private double frontrightPOWER = 0;
+        private double backleftPOWER = 0;
+        private double backrightPOWER = 0;
+        private double maxPOWER = 1;
 
 
-    @Override
-    public void start()
-    {
+        @Override
+        public void init() {
+            telemetry.addData("Readiness", "NOT READY TO START, PLEASE WAIT");
+            updateTelemetry(telemetry);
 
-    }
+            robot.init(hardwareMap);
 
-    @Override
-    public void loop(){
-        //*****************
-        //Game Controller 1
-        //*****************
+            telemetry.addData("Readiness", "Press Play to start");
+            //telemetry.addData("If you notice this", "You are COOL!!! (Evan was here)");
+            updateTelemetry(telemetry);
+        }
 
-        //Read controller input
-        //Left and right are opposite; front and back are same
-        leftGP1Y = -gamepad1.left_stick_y;
-        //leftGP1X = gamepad1.left_stick_x;
-        //rightGP1X = gamepad1.right_stick_x;
-        rightGP1Y = gamepad1.right_stick_y;
+        @Override
+        public void init_loop()
+        {
 
-        //remove slight touches
-        if (Math.abs(leftGP1Y) < 0.05) {
+        }
+
+
+        @Override
+        public void start()
+        {
+
+        }
+
+        @Override
+        public void loop(){
+            //*****************
+            //Game Controller 1
+            //*****************
+
+
+            /*
+                    Y
+                  X   B
+                    A
+
+             */
+
+            //Read controller input
+            //Left and right are opposite; front and back are same
+            leftGP1Y = -gamepad1.left_stick_y;
+            //leftGP1X = gamepad1.left_stick_x;
+            //rightGP1X = gamepad1.right_stick_x;
+            rightGP1Y = -gamepad1.right_stick_y;
+
+            //remove slight touches
+            if (Math.abs(leftGP1Y) < 0.05) {
                 leftGP1Y = 0;
             }
-        if (Math.abs(leftGP1X) < 0.05) {
-            leftGP1X = 0;
+            if (Math.abs(leftGP1X) < 0.05) {
+                leftGP1X = 0;
+            }
+
+            if (Math.abs(rightGP1Y) < 0.05) {
+                leftGP1Y = 0;
+            }
+            if (Math.abs(rightGP1X) < 0.05) {
+                leftGP1X = 0;
+            }
+
+            //speeds of drive motors
+            frontleftPOWER = backleftPOWER = leftGP1Y;
+            frontrightPOWER = backrightPOWER = rightGP1Y;
+
+            maxPOWER = Math.abs(frontleftPOWER);
+            if (Math.abs(backleftPOWER) > maxPOWER) {
+                maxPOWER = Math.abs(backleftPOWER);
+            }
+            if (Math.abs(backrightPOWER) > maxPOWER) {
+                maxPOWER = Math.abs(backrightPOWER);
+            }
+            if (Math.abs(frontrightPOWER) > maxPOWER) {
+                maxPOWER = Math.abs(frontrightPOWER);
+            }
+
+            //setting powers to motors
+            robot.frontRightMotor.setPower(frontrightPOWER);
+            robot.frontLeftMotor.setPower(frontleftPOWER);
+            robot.backRightMotor.setPower(backleftPOWER);
+            robot.backLeftMotor.setPower(backrightPOWER);
+
+
         }
 
-        //speeds of drive motors
-        frontleftPOWER = backleftPOWER = leftGP1Y;
-        frontrightPOWER = backrightPOWER = rightGP1Y;
-
-        maxPOWER = Math.abs(frontleftPOWER);
-        if (Math.abs(backleftPOWER) > maxPOWER) {
-            maxPOWER = Math.abs(backleftPOWER);
-        }
-        if (Math.abs(backrightPOWER) > maxPOWER) {
-            maxPOWER = Math.abs(backrightPOWER);
-        }
-        if (Math.abs(frontrightPOWER) > maxPOWER) {
-            maxPOWER = Math.abs(frontrightPOWER);
-        }
-
-        if (maxPOWER > 1.0) {
-            frontrightPOWER = frontrightPOWER / maxPOWER;
-            frontleftPOWER = frontleftPOWER / maxPOWER;
-            backrightPOWER = backrightPOWER / maxPOWER;
-            backleftPOWER = backleftPOWER / maxPOWER;
-        }
 
 
     }
-}
+    
+    
+

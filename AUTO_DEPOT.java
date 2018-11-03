8b6bd969892cc64093dfba4b1073b8044772d6db
Csun01 <42942891+Csun01@users.noreply.github.com>
2018-11-03 13:54:08 -0500

    Add files via upload

--- AUTO_DEPOT.java
+++ AUTO_DEPOT.java
@@ -1,6 +1,34 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuMarkInstanceId;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+import java.util.Locale;
 
 @Autonomous(name = "Blue depot", group = "Tau")
 public class AUTO_DEPOT extends AUTO_METHODS {
@@ -11,21 +39,16 @@ public class AUTO_DEPOT extends AUTO_METHODS {
         setUp(hardwareMap);
 
         //Call other methods
-        unhang();
-        driveForward(0.5, Math.sqrt(2) * 24);
+        driveForward(0.5, Math.sqrt(2) * 36);
         sleepTau(2000);
-        turnDegrees(0.5, 45);
+        turnDegrees(0.5, 90);
         sleepTau(2000);
-        driveForward(0.5, 4.5 );
+        driveForward(0.5, Math.sqrt(2) * 12);
         sleepTau(2000);
         //calls arm method
-        turnDegrees(0.5, 90);
+        turnDegrees(0.5, 30);
         sleepTau(2000);
-        driveForward(0.5, 3);
-        sleepTau(2000);
-        turnDegrees(0.5, 90);
-        sleepTau(2000);
-        driveForward(0.75, 84);
+        driveForward(1, 30);
         sleepTau(5000);
 
 
